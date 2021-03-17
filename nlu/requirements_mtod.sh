7a4c4a0c245440033d13e9fb477a600ce7d85878
Meryem <mhamdi@adobe.com>
2021-03-16 21:49:39 -0700

    added pip requirements for MTOD

---
+++ nlu/requirements_mtod.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+conda install pytorch==1.6.0 torchvision cudatoolkit=10.1.243 -c pytorch
+pip install pytorch_transformers==1.2.0
+pip install pytorch_pretrained_bert==0.6.2
+pip install scikit-learn==0.23.2
+pip install transformers==3.4.0
+pip install torchviz==0.0.1
+pip install tensorboard==2.3.0
+pip install learn2learn==0.1.3
+pip install tqdm==4.50.0
